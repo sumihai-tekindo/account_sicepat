2d8b1083df28f294707314bd7f82acf3c3df53a6
timotiuswigianto <timotius@sicepat.com>
2017-02-08 16:46:39 +0700

    update email ambil data ke user bukan employee

--- stock_receipt/models/account_analytic.py
+++ stock_receipt/models/account_analytic.py
@@ -4,6 +4,6 @@ class account_analytic_account(osv.osv):
 	_inherit = "account.analytic.account"
 
 	_columns ={
-		"user_admin_id" : fields.many2one("hr.employee","User Admin"),
+		"user_admin_id" : fields.many2one("res.users","User Admin"),
 		"day_interval"	: fields.float("Interval for sending goods(days)"),
 	}
