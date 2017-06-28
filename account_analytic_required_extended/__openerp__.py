e5d1f6501def662917ce0569db5876873a9ae65a
Pambudi Satria <udi_blackf@yahoo.com>
2017-06-28 15:26:12 +0700

    [account_analytic_required_extended]: add new module [sicepat_erp]: remove required analytic account on account.invoice view

---
+++ account_analytic_required_extended/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2017 Pambudi Satria (<https://github.com/pambudisatria>).
+#    @author Pambudi Satria <pambudi.satria@yahoo.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Account Analytic Required Extended',
+    'version': '1.0',
+    'category': 'Accounting & Finance',
+    'description': """
+Account Analytic Required
+=========================
+
+Extended from account_analytic_required but add on account.invoice.line
+    """,
+    'author': "Pambudi Satria",
+    'website': "https://github.com/pambudisatria",
+    'depends': ['account_analytic_required'],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
