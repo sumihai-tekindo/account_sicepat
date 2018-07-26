f2f1a9bcfdf1dd73592a5d491ff4a63bb6e11874
Pambudi Satria <udi_blackf@yahoo.com>
2018-07-26 10:48:33 +0700

    [invoice_supplier_dept_seq]: fixing

--- invoice_supplier_dept_seq/models/purchase.py
+++ invoice_supplier_dept_seq/models/purchase.py
@@ -1,6 +1,46 @@
 from openerp import api
 from openerp.osv import osv, fields
 
+class purchase_line_invoice(osv.osv_memory):
+	_inherit = 'purchase.order.line_invoice'
+
+	def _make_invoice_by_partner(self, cr, uid, partner, orders, lines_ids, context=None):
+		"""
+			create a new invoice for one supplier
+			@param cr : Cursor
+			@param uid : Id of current user
+			@param partner : The object partner
+			@param orders : The set of orders to add in the invoice
+			@param lines : The list of line's id
+		"""
+		purchase_obj = self.pool.get('purchase.order')
+		account_jrnl_obj = self.pool.get('account.journal')
+		invoice_obj = self.pool.get('account.invoice')
+		name = orders and orders[0].name or ''
+		journal_id = account_jrnl_obj\
+			.search(cr, uid, [('type', '=', 'purchase')], context=None)
+		journal_id = journal_id and journal_id[0] or False
+		a = partner.property_account_payable.id
+		inv = {
+			'name': name,
+			'origin': name,
+			'type': 'in_invoice',
+			'journal_id': journal_id,
+			'reference': partner.ref,
+			'account_id': a,
+			'partner_id': partner.id,
+			'department_id': orders[0].department_id and orders[0].department_id.id or False,
+			'invoice_line': [(6, 0, lines_ids)],
+			'currency_id': orders[0].currency_id.id,
+			'comment': " \n".join([order.notes for order in orders if order.notes]),
+			'payment_term': orders[0].payment_term_id.id,
+			'fiscal_position': partner.property_account_position.id
+		}
+		inv_id = invoice_obj.create(cr, uid, inv, context=context)
+		purchase_obj.write(cr, uid, [order.id for order in orders], {'invoice_ids': [(4, inv_id)]}, context=context)
+		return inv_id
+
+
 class purchase_order(osv.osv):
 	_inherit ="purchase.order"
 
@@ -20,6 +60,19 @@ class purchase_order(osv.osv):
 		   :return: dict of value to create() the invoice
 		"""
 		res = super(purchase_order,self)._prepare_invoice( cr, uid, order, line_ids, context=context)
-		department_id=order.department_id and order.department_id.id or False
-		res.update({'department_id':department_id})
-		return  res
+		department_id = order.department_id and order.department_id.id or False
+		res.update({'department_id': department_id})
+		return res
+	
+	def action_picking_create(self, cr, uid, ids, context=None):
+		for order in self.browse(cr, uid, ids):
+			picking_vals = {
+				'picking_type_id': order.picking_type_id.id,
+				'partner_id': order.partner_id.id,
+				'date': order.date_order,
+				'origin': order.name,
+				'department_inv_id': order.department_id and order.department_id.id or False
+			}
+			picking_id = self.pool.get('stock.picking').create(cr, uid, picking_vals, context=context)
+			self._create_stock_moves(cr, uid, order, order.order_line, picking_id, context=context)
+		return picking_id
--- invoice_supplier_dept_seq/models/stock_invoice.py
+++ invoice_supplier_dept_seq/models/stock_invoice.py
@@ -23,25 +23,34 @@ from openerp.osv import osv, fields
 
 class stock_invoice_onshipping(osv.osv_memory):
     _inherit = "stock.invoice.onshipping"
-    _columns = {
-        'department_id': fields.many2one('account.invoice.department', 'Department', copy=False, ondelete='set null'),
-    }
+
     _defaults = {
         'invoice_date': fields.date.context_today,
     }
 
-    def create_invoice(self, cr, uid, ids, context=None):
-        context = dict(context or {})
-        data = self.browse(cr, uid, ids[0], context=context)
-        context['dept_id'] = data.department_id.id
-        return super(stock_invoice_onshipping, self).create_invoice(cr, uid, ids, context=context)
 
 class stock_picking(osv.osv):
     _inherit = 'stock.picking'
 
+    _columns = {
+        'department_inv_id': fields.many2one('account.invoice.department', 'Department', states={'done': [('readonly', True)], 'cancel': [('readonly', True)]}, ondelete='set null'),
+    }
+    
     def _get_invoice_vals(self, cr, uid, key, inv_type, journal_id, move, context=None):
-        if context is None:
-            context = {}
-        res = super(stock_picking, self)._get_invoice_vals(cr, uid, key, inv_type, journal_id, move, context=context)
-        res['department_id'] = context.get('dept_id', False)
-        return res
+        inv_vals = super(stock_picking, self)._get_invoice_vals(cr, uid, key, inv_type, journal_id, move, context=context)
+        if move.picking_id and move.picking_id.department_id:
+            department = move.picking_id.department_id
+            inv_vals.update({
+                'department_id': department.id,
+            })
+        return inv_vals
+
+class stock_quant(osv.osv):
+    _inherit = 'stock.quant'
+    
+    def _prepare_account_move_line(self, cr, uid, move, qty, cost, credit_account_id, debit_account_id, context=None):
+        move_lines = super(stock_quant, self)._prepare_account_move_line(cr, uid, move, qty, cost, credit_account_id, debit_account_id, context=context)
+        department_id = move.picking_id and move.picking_id.department_id and move.picking_id.department_id.id or False
+        for line in move_lines:
+            line[2]['department_id'] = department_id
+        return move_lines
