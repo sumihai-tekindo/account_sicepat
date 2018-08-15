a23c1244e8eb25660c056c50432386fb8ae51000
Adityapfm99 <adityapfm99@gmail.com>
2018-08-15 16:51:32 +0700

    update  branch,phone and address

--- maintenance_building/models/account_invoice.py
+++ maintenance_building/models/account_invoice.py
@@ -9,23 +9,38 @@ class account_invoice(models.Model):
 class asset_rental(models.Model):
 	_inherit ='asset.rental'
 
-	mobile= fields.Char("Mobile Phone")
-	phone = fields.Char("Phone")
-	street = fields.Text("Address")
+	@api.one
+	@api.depends('insurer_id')
+	def _compute_field1(self):
+		self.mobile = self.insurer_id.mobile
+		self.phone = self.insurer_id.phone
+		self.street = self.insurer_id.street
+
+	@api.one
+	@api.depends('insurer_id')  # if these fields are changed, call  method
+	def _check_change(self):
+		self.phone = self.insurer_id.phone
+
+	@api.one
+	@api.depends('insurer_id')  # if these fields are changed, call  method
+	def _check_change(self):
+		self.street = self.insurer_id.street	
+
+	@api.one
+	@api.depends('branch')  # if these fields are changed, call  method
+	def _check_changebranch(self):
+		self.alamat_lengkap = self.branch.alamat_lengkap
+
+	# mobile=fields.Char(compute=_compute_field1,store=True)
+	# phone = fields.Char(compute=_compute_field1,store=True)
+	# street = fields.Text(compute=_check_change,store=True)
+	# insurer_id = fields.Many2one('res.partner','Supplier')
+	# alamat_lengkap = fields.Text(compute=_check_changebranch,store=True)
+	# branch = fields.Many2one('account.analytic.account','Branch')
+
+	mobile=fields.Char(compute=_compute_field1,store=True)
+	phone = fields.Char(compute=_compute_field1,store=True)
+	street = fields.Text(compute=_check_change,store=True)
 	insurer_id = fields.Many2one('res.partner','Supplier')
-
-	@api.onchange('insurer_id')  # if these fields are changed, call  method
-	def check_change(self):
-		if self.insurer_id:
-			self.mobile = self.insurer_id.mobile
-
-
-	@api.onchange('insurer_id')  # if these fields are changed, call  method
-	def check_change1(self):
-		if self.insurer_id:
-			self.phone = self.insurer_id.phone
-
-	@api.onchange('insurer_id')  # if these fields are changed, call  method
-	def check_change2(self):
-		if self.insurer_id:
-			self.street = self.insurer_id.street
+	alamat_lengkap = fields.Text(compute=_check_changebranch,store=True)
+	branch = fields.Many2one('account.analytic.account','Branch')
