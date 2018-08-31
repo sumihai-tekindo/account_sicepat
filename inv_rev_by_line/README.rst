9595bc1a25c714998080989ac0019df93a0c4ba2
Pambudi Satria <udi_blackf@yahoo.com>
2018-08-31 08:34:50 +0700

    [inv_rev_by_line]: add ne w modules to create customer refund per customer invoice line. This commit not including the refund process yet.

---
+++ inv_rev_by_line/README.rst
@@ -0,0 +1,13 @@
+Customer Invoice Line Refund
+============================
+
+On open and paid invoice line, you can click on the "Create Refund" button. This
+will create a new revision of the invoice line.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Pambudi Satria <pambudi.satria@yahoo.com>
---
+++ inv_rev_by_line/models/account_invoice.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2018 Sicepat Ekspres Indonesia.
+#    @author Pambudi Satria <pambudi.satria@yahoo.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import fields, models, api
+from openerp.tools.translate import _
+
+class account_invoice(models.Model):
+    _inherit = "account.invoice.line"
+    
+    invoice_id = fields.Many2one('account.invoice', string='Invoice Reference',
+        ondelete='cascade', index=True, auto_join=True)
+    inv_line_origin_id = fields.Many2one('account.invoice.line', copy=False)
+    inv_line_rev_ids = fields.One2many('account.invoice.line', 'inv_line_origin_id')
+    invoice_type = fields.Selection(related='invoice_id.type', store=True, default=False, select=True)
+    invoice_state = fields.Selection(related='invoice_id.state', store=True, default=False, select=True)
